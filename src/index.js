3cb9520df556891b4b7292132dbd20dd9790250a
harry <harrypalmer889@gmail.com>
2020-05-21 17:09:17 +0100

    adding dob

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ const partialsPath = path.join(__dirname, '../templates/partials');
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
-
 app.use(express.static(publicDirectoryPath));
 app.use(express.static(path.join(__dirname, '/public')));
 
