49b8b53f13c95013847a60203720521505902fa8
Harry <harry@simpleclick.co.uk>
2020-05-27 11:21:48 +0100

    more crying

--- public/antithingy.js
+++ public/antithingy.js
@@ -1,28 +1,19 @@
-$(document).ready(() => {
-    let counter = 1;
-    $('#button-back').addClass('hidden');
-    $('#form-' + counter).addClass('viewed');
 
-    $('#button-next').click(() => {
-        $('#form-' + counter).removeClass('viewed')
-        counter++
-        if (counter > 1 ){
-        $('#button-back').removeClass('hidden');
-    }
-        console.log(counter)
-        $('#form-' + counter).addClass('viewed')
-        
-    })
+// this doesn't work and you need to find a better way of doing this :) 
 
-    $('#button-back').click(() => {
-        $('#form-' + counter).removeClass('viewed')
-        counter--
-        if (counter == 1){
-                $('#button-back').addClass('hidden');
-            }
-        $('#form-' + counter).addClass('viewed')
-        console.log(counter)
-        
-    })
- 
-});
+// let firstNameInput = document.getElementById("firstNameInput"),
+//     thingy = "thingy";
+
+
+//     function validatePassword(){
+//         if(firstNameInput.value != thingy.value) {
+//           confirm_password.setCustomValidity("Passwords Don't Match");
+//         } else {
+//           confirm_password.setCustomValidity('');
+//         }
+//       }
+      
+//       password.onchange = validatePassword;
+
+// firstNameInput.onchange = validateThingy;
+
--- public/signup.js
+++ public/signup.js
@@ -31,23 +31,4 @@ $(document).ready(() => {
         }
 
     })
-    // https://codepen.io/diegoleme/pen/surIK
 });
-
-// this doesn't work and you need to find a better way of doing this :) 
-
-// let firstNameInput = document.getElementById("firstNameInput"),
-//     thingy = "thingy";
-
-
-//     function validatePassword(){
-//         if(firstNameInput.value != thingy.value) {
-//           confirm_password.setCustomValidity("Passwords Don't Match");
-//         } else {
-//           confirm_password.setCustomValidity('');
-//         }
-//       }
-      
-//       password.onchange = validatePassword;
-
-// firstNameInput.onchange = validateThingy;
