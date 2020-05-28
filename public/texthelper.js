fd631efd1f4e2d14883df4ee5191a17108f4368b
harry <harrypalmer889@gmail.com>
2020-05-28 18:22:54 +0100

    actual progress on email input!

--- public/texthelper.js
+++ public/texthelper.js
@@ -26,17 +26,31 @@ document
     .addEventListener("input", backwardsText);
 
 
-const inputKeys = {
-    A: "A",
-    B: "B",
-    C: "C"
+const letters = [
+    "A","B","C","D","E","F","G","H","I","J","K","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
+    "a","b","c","d","e","f","g","h","i","j","k","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
+    "0","1","2","3","4","5","6","7","8","9",
+    "@","."
+]
+
+let email1 = 1;
+let email2 = 1;
+let email3 = 1;
+
+let i = 0
+function btnLetterSwitch(clicked_id){
+    let elem = document.getElementById(clicked_id);
+    i++;
+    if (i >= letters.length) {
+        i = 0; 
+    }
+    elem.value=letters[i]
+}
+ 
+function moreLetters(e){
+    $('<input type="button"  onclick="btnLetterSwitch(this.id)" class="btn" id="email-3" value="A"></input>').insertBefore("#moreletters");
+
 }
 
-let i = -1;
 
-function change(){
-    var elem = document.getElementById("email-1");
-    if (elem.value=="Before") elem.value = "After";
-    else elem.value = "Before";
-}
 
