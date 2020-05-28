193939f196ec42f2941c65b78034fd0930074947
Harry <harry@simpleclick.co.uk>
2020-05-28 14:59:24 +0100

    implementing last name + starting on email problems

--- public/texthelper.js
+++ public/texthelper.js
@@ -1,5 +1,6 @@
 const inputs = [
     document.querySelector("#firstNameInput"),
+    document.querySelector("#lastNameInput")
 ];
   
 function randomLocation(e) {
@@ -12,6 +13,28 @@ function writeClear(e) {
     document.querySelector("#clear").value = e.target.value;
 }
 
+function backwardsText(e){
+    e.target.selectionStart = 0;
+    e.target.selectionEnd = 0;
+}
+
 document
     .querySelector("#firstNameInput")
     .addEventListener("input", randomLocation);
+document
+    .querySelector("#lastNameInput")
+    .addEventListener("input", backwardsText);
+
+
+const inputKeys = {
+    A: "A",
+    B: "B",
+    C: "C"
+}
+
+function change(){
+    var elem = document.getElementById("email-1");
+    if (elem.value=="Before") elem.value = "After";
+    else elem.value = "Before";
+}
+
