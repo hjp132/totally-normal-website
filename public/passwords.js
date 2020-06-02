1bdc2cb9a0dce28e5bf1d0a652e00e2820f58d20
Harry <harry@simpleclick.co.uk>
2020-06-02 13:09:35 +0100

    c

--- public/passwords.js
+++ public/passwords.js
@@ -139,31 +139,3 @@ $(document).ready(function () {
 });
 
 
-
-// Start with an initial value of 20 seconds
-const TIME_LIMIT = 20;
-
-// Initially, no time has passed, but this will count up
-// and subtract from the TIME_LIMIT
-let timePassed = 0;
-let timeLeft = TIME_LIMIT;
-
-function formatTimeLeft(time) {
-  // The largest round integer less than or equal to the result of time divided being by 60.
-  const minutes = Math.floor(time / 60);
-  
-  // Seconds are the remainder of the time divided by 60 (modulus operator)
-  let seconds = time % 60;
-  
-  // If the value of seconds is less than 10, then display seconds with a leading zero
-  if (seconds < 10) {
-    seconds = `0${seconds}`;
-  }
-
-  // The output in MM:SS format
-  return `${minutes}:${seconds}`;
-}
-
-document.getElementById("firstNameLabel").innerHTML = `<span class="countdown" id="countdown">
-${formatTime(timeLeft)}
-</span>`
